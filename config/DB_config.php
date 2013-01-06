f910877be42affa5a4a762711ad9b18ae30adbff
Popa Stefan <stefanpopa0691@gmail.com>
2013-01-06 21:26:12 +0200

    Update config/DB_config.php

--- config/DB_config.php
+++ config/DB_config.php
@@ -4,7 +4,7 @@
 // DB connection config array >    
      $DB_config = [
                    "MySQL_DB_host" => "localhost",
-                   "MySQL_DB_name" => "1827-maindb",
+                   "MySQL_DB_name" => "maindb",
                    "MySQL_DB_user" => "stefanpopa",
                    "MySQL_DB_pass" => "stefanpopa",
                  ];
